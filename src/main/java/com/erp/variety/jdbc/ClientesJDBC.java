7ddbe13dc86efe7c2633fc8a6bbfa8b39518abd1
Kevin Santos <kevin.santos.solorzano@gmail.com>
2024-12-27 23:47:00 -0600

    Se agrega opcion de crear, editar y eliminar clientes y se agrega todas las variables de la tabla Clientes en la clase Clientes.java

--- src/main/java/com/erp/variety/jdbc/ClientesJDBC.java
+++ src/main/java/com/erp/variety/jdbc/ClientesJDBC.java
@@ -20,7 +20,12 @@ public class ClientesJDBC extends AbstractJDBC{
 		Connection conn = sconn.getConnection();
 		Statement st = null;
 		ResultSet rs;
-		String query = "SELECT IdCliente, nombreCliente FROM dbo.Cliente";
+		String query = "SELECT IdCliente, nombreCliente, nombreComercial, grupoCliente,\r\n"
+				+ "DireccionCliente, PaisCliente, DepartamentoCliente, MunicipioCliente, \r\n"
+				+ "TelefonoCliente, CelularCliente, EmailCliente, NrcCliente, NitCliente, DuiCliente,\r\n"
+				+ "GiroCliente, ContactoCliente, SitiowebCliente, WhatsappCliente, Latitud, Longitud, \r\n"
+				+ "CtaContableCliente, IdEmpleado, Activo "
+				+ "FROM dbo.Cliente";
 		try {
 			st = conn.createStatement();
 			rs = st.executeQuery(query);
@@ -52,7 +57,12 @@ public class ClientesJDBC extends AbstractJDBC{
 		Connection conn = sconn.getConnection();
 		Statement st = null;
 		ResultSet rs;
-		String query = "SELECT nombreCliente FROM dbo.Cliente";
+		String query = "SELECT IdCliente, nombreCliente, nombreComercial, grupoCliente,\r\n"
+				+ "DireccionCliente, PaisCliente, DepartamentoCliente, MunicipioCliente, \r\n"
+				+ "TelefonoCliente, CelularCliente, EmailCliente, NrcCliente, NitCliente, DuiCliente,\r\n"
+				+ "GiroCliente, ContactoCliente, SitiowebCliente, WhatsappCliente, Latitud, Longitud, \r\n"
+				+ "CtaContableCliente, IdEmpleado, Activo "
+				+ "FROM dbo.Cliente";
 		try {
 			st = conn.createStatement();
 			rs = st.executeQuery(query);
@@ -77,7 +87,52 @@ public class ClientesJDBC extends AbstractJDBC{
 	@Override
 	public void save(Object entity) throws SQLException {
 		Clientes clientes = (Clientes) entity;
-		
+		SqlConn sconn = new SqlConn();
+		Connection conn = sconn.getConnection();
+		String query = "INSERT INTO dbo.Cliente(IdCliente,nombreCliente, nombreComercial, grupoCliente,\r\n"
+				+ "DireccionCliente, PaisCliente, DepartamentoCliente, MunicipioCliente, \r\n"
+				+ "TelefonoCliente, CelularCliente, EmailCliente, NrcCliente, NitCliente, DuiCliente,\r\n"
+				+ "GiroCliente, ContactoCliente, SitiowebCliente, WhatsappCliente, Latitud, Longitud, \r\n"
+				+ "CtaContableCliente, IdEmpleado, Activo)\r\n"
+				+ "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		try {
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, clientes.getIdCliente());
+			ps.setString(2, clientes.getNombreCliente());
+			ps.setString(3, clientes.getNombreComercial());
+			ps.setInt(4, clientes.getGrupoCliente());
+			ps.setString(5, clientes.getDireccionCliente());
+			ps.setString(6, clientes.getPaisCliente());
+			ps.setString(7, clientes.getDepartamentoCliente());
+			ps.setString(8, clientes.getMunicipioCliente());
+			ps.setString(9, clientes.getTelefonoCliente());
+			ps.setString(10, clientes.getCelularCliente());
+			ps.setString(11, clientes.getEmailCliente());
+			ps.setString(12, clientes.getNrcCliente());
+			ps.setString(13, clientes.getNitCliente());
+			ps.setString(14, clientes.getDuiCliente());
+			ps.setString(15, clientes.getGiroCliente());
+			ps.setString(16, clientes.getContactoCliente());
+			ps.setString(17, clientes.getSitioWebCliente());
+			ps.setString(18, clientes.getWhatsappCliente());
+			ps.setString(19, clientes.getLatitud());
+			ps.setString(20, clientes.getLongitud());
+			ps.setString(21, clientes.getCtaContableCliente());
+			ps.setInt(22, clientes.getIdEmpleado());
+			ps.setInt(23, clientes.getActivo());
+
+			ps.executeUpdate();
+			conn.commit();
+		} catch (Exception e) {
+			conn.rollback();
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -85,11 +140,55 @@ public class ClientesJDBC extends AbstractJDBC{
 		Clientes clientes = (Clientes) entity;
 		SqlConn sconn = new SqlConn();
 		Connection conn = sconn.getConnection();
-		String query = "";
+		String query = "UPDATE dbo.Cliente\r\n"
+				+ "SET nombreCliente= ? \r\n"
+				+ "nombreComercial= ? \r\n"
+				+ "grupoCliente= ?\r\n"
+				+ "DireccionCliente= ? \r\n"
+				+ "PaisCliente= ? \r\n"
+				+ "DepartamentoCliente= ? \r\n"
+				+ "MunicipioCliente= ? \r\n"
+				+ "TelefonoCliente= ? \r\n"
+				+ "CelularCliente= ? \r\n"
+				+ "EmailCliente= ? \r\n"
+				+ "NrcCliente= ? \r\n"
+				+ "NitCliente= ? \r\n"
+				+ "DuiCliente= ?\r\n"
+				+ "GiroCliente= ? \r\n"
+				+ "ContactoCliente= ? \r\n"
+				+ "SitiowebCliente= ? \r\n"
+				+ "WhatsappCliente= ? \r\n"
+				+ "Latitud= ? \r\n"
+				+ "Longitud= ? \r\n"
+				+ "CtaContableCliente= ? \r\n"
+				+ "IdEmpleado= ? \r\n"
+				+ "Activo = ?\r\n"
+				+ "WHERE IdCliente = ?";
 		try {
 			PreparedStatement ps = conn.prepareStatement(query);
-			//ps.setString(1, clientes.getToken());
-
+			ps.setString(1, clientes.getNombreCliente());
+			ps.setString(2, clientes.getNombreComercial());
+			ps.setInt(3, clientes.getGrupoCliente());
+			ps.setString(4, clientes.getDireccionCliente());
+			ps.setString(5, clientes.getPaisCliente());
+			ps.setString(6, clientes.getDepartamentoCliente());
+			ps.setString(7, clientes.getMunicipioCliente());
+			ps.setString(8, clientes.getTelefonoCliente());
+			ps.setString(9, clientes.getCelularCliente());
+			ps.setString(10, clientes.getEmailCliente());
+			ps.setString(11, clientes.getNrcCliente());
+			ps.setString(12, clientes.getNitCliente());
+			ps.setString(13, clientes.getDuiCliente());
+			ps.setString(14, clientes.getGiroCliente());
+			ps.setString(15, clientes.getContactoCliente());
+			ps.setString(16, clientes.getSitioWebCliente());
+			ps.setString(17, clientes.getWhatsappCliente());
+			ps.setString(18, clientes.getLatitud());
+			ps.setString(19, clientes.getLongitud());
+			ps.setString(20, clientes.getCtaContableCliente());
+			ps.setInt(21, clientes.getIdEmpleado());
+			ps.setInt(22, clientes.getActivo());
+			ps.setString(23, clientes.getIdCliente());
 
 			ps.executeUpdate();
 			conn.commit();
@@ -109,7 +208,27 @@ public class ClientesJDBC extends AbstractJDBC{
 	@Override
 	public void delete(Object entity) throws SQLException {
 		Clientes clientes = (Clientes) entity;
-		
+		SqlConn sconn = new SqlConn();
+		Connection conn = sconn.getConnection();
+		String query = "DELETE FROM dbo.Cliente\r\n"
+				+ "WHERE IdCliente = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, clientes.getIdCliente());
+
+
+			ps.executeUpdate();
+			conn.commit();
+		} catch (Exception e) {
+			conn.rollback();
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- src/main/java/com/erp/variety/model/Clientes.java
+++ src/main/java/com/erp/variety/model/Clientes.java
@@ -11,4 +11,25 @@ public class Clientes {
 
 	public String idCliente;
 	public String nombreCliente;
+	public String nombreComercial;
+	public int grupoCliente;
+	public String direccionCliente;
+	public String paisCliente;
+	public String departamentoCliente;
+	public String municipioCliente;
+	public String telefonoCliente;
+	public String celularCliente;
+	public String emailCliente;
+	public String nrcCliente;
+	public String nitCliente;
+	public String duiCliente;
+	public String giroCliente;
+	public String contactoCliente;
+	public String sitioWebCliente;
+	public String whatsappCliente;
+	public String latitud;
+	public String longitud;
+	public String ctaContableCliente;
+	public int idEmpleado;
+	public int activo;
 }
